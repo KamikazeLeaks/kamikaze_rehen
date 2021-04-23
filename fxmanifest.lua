a76f9abb96b13c8cacb3eebe9d72460f1928386d
lizarragaeneko <67381650+lizarragaeneko@users.noreply.github.com>
2021-04-23 20:25:37 +0200

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,9 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'KAMIKAZELEAKS'
+description '/rehen - KAMIKAZELEAKS'
+version '1.0.0'
+
+client_script "cl_kamikaze.lua"
+server_script "sv_kamikaze.lua"
